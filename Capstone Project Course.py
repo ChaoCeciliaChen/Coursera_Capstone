26788210176d87c5df31771d58f3bfdfe30e035c
CC <gms55515@hotmail.com>
2019-04-17 21:18:42 -0700

    testing

---
+++ Capstone Project Course.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# # This is Capstone Project Course
+
+# This notebook is mainly used for the capstone project.
+# 
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print(' Hello Capstone Project Course!')
+
